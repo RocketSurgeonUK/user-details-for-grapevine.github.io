84550667bccc328e70db02f9456a95664e4915dd
Oliver - Rocket Surgeon <Oliver@RocketSurgeon.co.uk>
2020-08-05 10:24:54 +0100

    Updated Users

--- Users.js
+++ Users.js
@@ -7,7 +7,10 @@
         {"id":6, "name": "Some six", "email": "Somesix@Someone.com", "subject": "one two three", "message": "This is a test 6" },
         {"id":7, "name": "Some seven", "email": "Someseven@Someone.com", "subject": "one two three", "message": "This is a test 7" },
         {"id":8, "name": "Some eight", "email": "Someseight@Someone.com", "subject": "one two three", "message": "This is a test 8" },
-        {"id":9, "name": "Some nine", "email": "Somenine@Someone.com", "subject": "one two three", "message": "This is an automated message" }
+        {"id":9, "name": "Some nine", "email": "Somenine@Someone.com", "subject": "one two three", "message": "This is an automated message" },
+
+        {"id":10, "name": "Some ten", "email": "Someten@Someone.com", "subject": "one two three", "message": "This is an automated message!" }
+
         
         
 ]}
