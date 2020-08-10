c20bdd825d9eb00d110f25905f1555f23a5a9821
Oliver - Rocket Surgeon <Oliver@RocketSurgeon.co.uk>
2020-08-10 20:16:41 +0100

    changed structure to match ATRIIS

--- Users.js
+++ Users.js
@@ -1,15 +1,3 @@
 { "data": [
-        {"id":1, "name": "Some one", "email": "Someone@Someone.com", "subject": "one two three", "message": "This is a test" },
-        {"id":2, "name": "Some two", "email": "Sometwo@Someone.com", "subject": "one two three", "message": "This is a test" },
-        {"id":3, "name": "Some three", "email": "Somethree@Someone.com", "subject": "one two three", "message": "This is a test" },
-        {"id":4, "name": "Some four", "email": "Somefour@Someone.com", "subject": "one two three", "message": "This is a test" },
-        {"id":5, "name": "Some five", "email": "Somefive@Someone.com", "subject": "one two three", "message": "This is a test 5" },
-        {"id":6, "name": "Some six", "email": "Somesix@Someone.com", "subject": "one two three", "message": "This is a test 6" },
-        {"id":7, "name": "Some seven", "email": "Someseven@Someone.com", "subject": "one two three", "message": "This is a test 7" },
-        {"id":8, "name": "Some eight", "email": "Someseight@Someone.com", "subject": "one two three", "message": "This is a test 8" },
-        {"id":9, "name": "Some nine", "email": "Somenine@Someone.com", "subject": "one two three", "message": "This is a test 9" },
-        {"id":10, "name": "Some ten", "email": "Someten@Someone.com", "subject": "one two three", "message": "This is a test 10" },
-
-        {"id":11, "name": "test 11", "email": "test11@Someone.com", "subject": "one two three", "message": "This is an automated message!!!" }
-
+        {"id":1, "Destination": "Paris,France,(PAR)", "CheckIn": "2020-11-01", "CheckOut": "2020-12-02" }
 ]}
