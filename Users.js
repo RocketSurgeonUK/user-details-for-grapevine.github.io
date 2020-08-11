53c51c7046f011706922bf1b0e9db88f226eb6da
Oliver - Rocket Surgeon <Oliver@RocketSurgeon.co.uk>
2020-08-11 01:41:42 +0100

    changed users

--- Users.js
+++ Users.js
@@ -1,3 +1,3 @@
 { "data": [
-        {"id":1, "Destination": "Paris,France,(PAR)", "CheckIn": "2020-11-01", "CheckOut": "2020-12-02" }
+        {"id":1, "Destination": "Paris,France", "CheckIn": "2020-11-01", "CheckOut": "2020-12-02" }
 ]}
